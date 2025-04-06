bf31d56d187b6840a64b31db06c3e28d555fbd26
Gabriel Morais <40728419+Funarix@users.noreply.github.com>
2025-04-06 07:00:08 +0000

    nova atualização

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
 
-    return<h1>Karol, fiz esse site só pra dizer pra você, e pra todo mundo, que meu coração queima de amor por você!💙</h1>
+    return<h1>Karol, fiz esse site só pra dizer pra você, e pra todo mundo, que meu coração queima de amor por ti! Você é a mulher da minha vida e estarei sempre do seu lado. Te amo!💙</h1>
 
     
 
