02dd8bd68874d5681913fcd792bcdffc13b521c9
Gabriel Morais <40728419+Funarix@users.noreply.github.com>
2025-04-06 05:50:14 +0000

    Alteração no index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
 
-    return<h1>Karol, eu te amo!!</h1>
+    return<h1>Karol, fiz esse site só pra dizer pra você, e pra todo mundo, que meu coração queima de amor por você!💙</h1>
 }
 
 export default Home
