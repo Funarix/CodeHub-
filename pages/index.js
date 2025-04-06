3129b93f12844a11019e92e7f5d44ee41c58a97b
Gabriel Morais <40728419+Funarix@users.noreply.github.com>
2025-04-06 06:46:06 +0000

    novas alteracoes

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
-function Home() {
-    return (
-      <div>
-        <h1>
-          Karol, fiz esse site só pra dizer pra você, e pra todo mundo, que meu coração queima de amor por você! 💙
-        </h1>
-        <img src="/assets/karol.jpeg" alt="casalzinho" width="200" />
-      </div>
-    );
-  }
-  
-  export default Home;
-  
+function Home(){
+
+    return<h1>Karol, fiz esse site só pra dizer pra você, e pra todo mundo, que meu coração queima de amor por você!💙</h1>
+
+    
+
+
+}
+
+export default Home
