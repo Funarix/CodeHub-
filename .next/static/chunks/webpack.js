f9927b1907abc821ab5cc8f6115620d68afde61b
Gabriel Morais <40728419+Funarix@users.noreply.github.com>
2025-04-13 02:52:40 +0000

    install `jest` and configure tests scripts

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1e1587e4462f5df1"; }
+/******/ 		__webpack_require__.h = function() { return "cbe342bfe2ab47c2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.894b7a15e31296aa.hot-update.js
+++ .next/static/webpack/webpack.894b7a15e31296aa.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1e1587e4462f5df1"; }
+/******/ 	__webpack_require__.h = function() { return "8e1548fd0ea01592"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/make namespace object */
